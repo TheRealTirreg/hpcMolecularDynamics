7e7435015eaa20c5cd81b43ac7f1d1ff6fff6d67
Gerrit Laptop <gerrit.freiwald@gmail.com>
2023-06-23 13:40:11 +0200

    plot file for ms05

--- milestones/05/plot.py
+++ milestones/05/plot.py
@@ -1,35 +1,39 @@
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
 import matplotx
 import tikzplotlib
 
 # Prepare data
-labels = [r"$E_\mathrm{pot}$", r"$E_\mathrm{kin}$", r"$E_\mathrm{total}$"]
-headers = ["Epot", "Ekin", "Etotal"]
+labels = ["With Neighborlist", "No Neighborlist"]
+headers = ["n", "With Neighborlist", "No Neighborlist"]
 
 # Import Data
-df = pd.read_csv('ovito/energy.csv', header=None, sep="\t")
+df = pd.read_csv('ovito/perf.csv', header=None, sep=",")
 df.columns = headers
+print(df)
 
 # Draw Plot
-mycolors = ['tab:pink', 'tab:blue', 'tab:green']
+mycolors = ['tab:pink', 'tab:blue']
 # ['tab:orange', 'tab:brown', 'tab:grey', 'tab:red',
 # 'tab:olive', 'deeppink', 'steelblue', 'firebrick', 'mediumseagreen']
 
 plt.figure(figsize=(8, 5), dpi=80)
 
-for i in range(3):
+for i in range(2):
     print(df.shape, "\n")
-    plt.plot(df[headers[i]], color=mycolors[i], label=labels[i])
+    plt.plot(df[headers[0]], df[headers[i+1]], color=mycolors[i], label=labels[i])
 
 # Decoration
-plt.ylim(-200, 60)
-plt.xlim(0, 100)
-plt.xlabel(r'Time in $\sqrt{m \sigma^2 / \varepsilon}$')
-plt.ylabel(r'Energy in $eV$')
-plt.yticks(fontsize=12, alpha=.7)
-plt.title("Energy Conservation", fontsize=22)
-matplotx.line_labels()
+plt.ylim(0, 1000)
+plt.xlim(0, 1000)
+plt.xticks(df[headers[0]])
+plt.xticks(fontsize=12, alpha=.7)
+plt.xlabel(r'Number of Atoms')
+plt.ylabel(r'Execution time in $s$')
+plt.title("Execution time MS05", fontsize=22)
+plt.legend(loc="upper left")
+# matplotx.line_labels()
 
 # Plot grid behind graphs
 plt.grid(axis='y', alpha=.3)
@@ -40,7 +44,7 @@ plt.gca().spines["bottom"].set_alpha(0.5)
 plt.gca().spines["right"].set_alpha(0.0)
 plt.gca().spines["left"].set_alpha(0.5)
 
-tikzplotlib.save(r"ovito/plot.tex")
+# tikzplotlib.save(r"ovito/plot.tex")
 plt.savefig(r"ovito/plot.png")
 
 plt.show()  # clears the plot
